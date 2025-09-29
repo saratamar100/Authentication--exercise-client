67c31add5807daa5b31105caa98418c3737e1126
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-09-29 11:21:11 +0300

    step 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { getBooks } from "./services/books";
+import { getBooks, login } from "./services/books";
 
 function App() {
   const [books, setBooks] = useState([]);
@@ -8,10 +8,13 @@ function App() {
     getBooks().then((b) => setBooks(b));
   }, []);
   console.log("books", books);
+  const handleLogin = async () => {login("sara", "p1")};
   return (
     <div className="App">
       <h1>Authentication Exercise</h1>
       <div>{JSON.stringify(books)}</div>
+
+      <button onClick={handleLogin}>Login with username "sara" and password "p1"</button>
     </div>
   );
 }
--- src/services/books.js
+++ src/services/books.js
@@ -1,4 +1,12 @@
-import { httpGet } from "./http";
+import { httpGet, httpPost } from "./http";
 export async function getBooks() {
   return httpGet("/books");
+}
+export async function login(username, password) {
+  return httpPost("/login", { username, password }).then((res) => {
+    if (res.token) {
+      localStorage.setItem("token", res.token);
+    } 
+    return res;
+  });
 }
